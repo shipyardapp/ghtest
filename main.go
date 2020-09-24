54b1c841af545889e5e4be8e936238dde5e498af
forstmeier <john@shipyardapp.com>
2020-09-24 11:12:44 -0400

    Add ZIP URL parse/fetch logic

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 
 	"github.com/google/go-github/v32/github"
 )
@@ -26,5 +29,28 @@ func main() {
 
 	for _, tag := range tags {
 		fmt.Printf("tag: %+v\n", tag)
+		zipURL := *tag.ZipballURL
+		fmt.Println("zip url:", zipURL)
+
+		resp, err := http.Get(zipURL)
+		if err != nil {
+			fmt.Printf("error: %s", err.Error())
+		}
+		defer resp.Body.Close()
+		fmt.Println("status:", resp.Status)
+		if resp.StatusCode != 200 {
+			fmt.Printf("error: %s", err.Error())
+		}
+
+		out, err := os.Create("ghtest_repo.zip")
+		if err != nil {
+			fmt.Printf("error: %s", err.Error())
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			fmt.Printf("error: %s", err.Error())
+		}
 	}
 }
