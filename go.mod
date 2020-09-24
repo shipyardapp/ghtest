2a66e101236b67b61876181787fd453d99ae86b3
forstmeier <john@shipyardapp.com>
2020-09-24 10:52:46 -0400

    Add initial test logic

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/shipyardapp/ghtest
+
+go 1.15
+
+require github.com/google/go-github/v32 v32.1.0
--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/v32/github"
+)
+
+func main() {
+	client := github.NewClient(nil)
+
+	orgs, _, err := client.Organizations.List(context.Background(), "forstmeier", nil)
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+	}
+
+	for _, org := range orgs {
+		fmt.Printf("org: %+v\n", org)
+	}
+}
